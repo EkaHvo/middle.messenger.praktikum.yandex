fe1f06fab5dcbc72a07fc8e49afdec4de6b63138
@EkaHvo <ekasagalaeva@gmail.com>
2022-10-05 12:33:56 +0300

    3_sprint fix review-4 add history-fallback

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const fallback = require('express-history-api-fallback');
+const express = require('express');
 const app = express();
+const path = require('path');
+const root = path.resolve(__dirname, 'dist');
+app.use(express.static(root));
+app.use(fallback('index.html', { root: root }));
+
 const PORT = 3000;
 process.env.PORT || PORT;
-
-const root = `${__dirname}/dist`
-app.use(express.static(root));
-app.use(fallback('index.html', { root }))
-
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
